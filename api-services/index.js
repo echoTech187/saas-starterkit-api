bcbacc2d190ebc46075b068eff90474ad4dec352
Pongo <echotech8310@gmail.com>
2026-01-31 19:34:48 +0700

    updata cors

--- api-services/index.js
+++ api-services/index.js
@@ -8,12 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
+app.use('/public', express.static('static'));
 
 app.use('/api/v1', memberRouter);
 
